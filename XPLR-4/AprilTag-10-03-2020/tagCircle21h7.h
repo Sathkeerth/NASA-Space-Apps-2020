3f8c1fbcb9545bd9b74c6fd15d5b2fffacd1d93b
Sathkeerth <67965770+Sathkeerth@users.noreply.github.com>
2020-10-04 22:44:45 +0530

    Add files via upload
    *** April Tags Generated ***

---
+++ XPLR-4/AprilTag-10-03-2020/tagCircle21h7.h
@@ -0,0 +1,17 @@
+#ifndef _TAGCircle21H7
+#define _TAGCircle21H7
+
+#include "apriltag.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+apriltag_family_t *tagCircle21h7_create();
+void tagCircle21h7_destroy(apriltag_family_t *tf);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
